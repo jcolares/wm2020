94594edf4152d2b1d37acff0bb10da717b950fb3
jcolares <jcolares@gmail.com>
2020-07-26 23:52:22 -0300

    ajustes para execução no servidor

--- train_unet_resnet18.py
+++ train_unet_resnet18.py
@@ -1,13 +1,14 @@
 #from preprocessing import json_2_bitmap, resize_320x240, list_files, preprocess_file
 from preprocessing import list_files
 from training import *
-
 import segmentation_models_pytorch as smp
 
 
 global df, img_dir, mask_dir, mean, std
-img_dir = '/home/jeff/datasets/SPD/train_img'
-mask_dir = '/home/jeff/datasets/SPD/train_mask'
+#img_dir = '/home/jeff/datasets/SPD/train_img'
+#mask_dir = '/home/jeff/datasets/SPD/train_mask'
+img_dir = 'data/train_img'
+mask_dir = 'data/train_mask'
 
 # imagenet mean/std will be used as the resnet backbone is trained on imagenet stats
 mean, std = (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)
